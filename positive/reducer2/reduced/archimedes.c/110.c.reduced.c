4b2df8224c68dbce2ec74ca4244b79641206cc48
Kwonsoo <kwonsoo.chae@gmail.com>
2016-03-13 22:55:28 +0900

    more reduced

---
+++ positive/reducer2/reduced/archimedes.c/110.c.reduced.c
@@ -0,0 +1,12 @@
+double EDGE[1][617];
+Read_Input_File() {
+  register i = 1;
+  while (1) {
+    if (!(i <= 50))
+      goto while_break___0;
+    airac_observe(EDGE[1], i);
+    i++;
+  }
+while_break___0:
+  ;
+}
---
+++ positive/reducer2/reduced/archimedes.c/115.c.reduced.c
@@ -0,0 +1,11 @@
+int nx, ny;
+int i_dom[1][309];
+main() {
+  Read_Input_File();
+  int i, j = 1;
+  if (nx)
+    j = ny;
+  airac_observe(i_dom[i], j);
+}
+
+Read_Input_File() { ny = 50; }
