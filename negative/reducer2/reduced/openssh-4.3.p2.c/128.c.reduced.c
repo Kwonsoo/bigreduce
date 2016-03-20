21369e6a5934378eb6c1d678c4aef2cfdd5fa24d
Kwonsoo <kwonsoo.chae@gmail.com>
2016-03-20 17:39:21 +0900

    more reduced

---
+++ negative/reducer2/reduced/openssh-4.3.p2.c/128.c.reduced.c
@@ -0,0 +1,11 @@
+void *mm_make_entry_tmp___0;
+mmtree_RB_INSERT_COLOR(p1) {
+  int tmp;
+  airac_observe(tmp, 0);
+  tmp = p1;
+}
+
+mm_create() {
+  mm_make_entry_tmp___0 = xmalloc();
+  mmtree_RB_INSERT_COLOR(mm_make_entry_tmp___0);
+}
